354fbfcde795481ad5d81a2c3d00e3545288f27b
ahg223 <ahg2230@gmail.com>
2019-09-04 01:51:05 +0900

    proto_ver1

--- Data_Creator.py
+++ Data_Creator.py
@@ -5,7 +5,8 @@ import argparse
 from PIL import Image
 import os
 import secrets
-
+from copy import deepcopy
+import numpy as np
 
 class DD_gatherer:
     def __init__(self):
@@ -60,7 +61,7 @@ class DD_gatherer:
 
     def get_minimap_noise(self):
         local = self.local + "noise/"
-        local_big = self.local + "ring/"
+        local_big = self.local + "ping/"
         url = self.latest_url + "game/data/menu/minimapicons/"
 
         with urllib.request.urlopen(url) as page:
@@ -69,7 +70,7 @@ class DD_gatherer:
 
             for link in page.find_all("a"):
                 name = link.get("href")
-                Test_icon = ["bard", "palisades", "leblanc", "odyssey", "victimcounter", "yorick", "test", "tt", "as", "center", "cool", "cry", "dark", "destroy", "doom", "enemyp", "energy", "neutral", "friendly", "health", "king", "cp", "quest", "sg", "cannon", "slime", "sre", "vilemaw", "vo"]
+                Test_icon = ["bard", "palisades", "odyssey", "victimcounter", "yorick", "test", "tt", "as", "center", "cool", "cry", "dark", "destroy", "doom", "enemyp", "energy", "neutral", "friendly", "health", "king", "cp", "quest", "sg", "cannon", "slime", "sre", "vilemaw", "vo"]
                 if ".png" not in name: continue
                 Flag = False
                 for error in Test_icon:
@@ -94,14 +95,27 @@ class DD_gatherer:
                     except: continue
 
 class DataCreator:
-    def __init__(self, save, amount, noise, overlap):
+    def __init__(self, save, amount, noise, overlap, ping):
         self.save = save
         self.amount = amount
-        self.noise = noise
+        self.noise_len = noise
         self.overlap = overlap
+        self.ping_len = ping
+        self.DATA = []
 
-    def saving():
-        path = "./Data/" 
+    def stack(self):
+        if self.save == "png": self.DATA.append(deepcopy(self.ping))
+        elif self.save == "npy": self.DATA.append(np.array(self.ping))
+
+    def saving(self):
+        if self.save == "png":
+            count = 0
+            for img in self.DATA:
+                img.save("./Data/"+ str(count) + ".png")
+                count += 1
+                
+        elif self.save == "npy": np.save("./Data/Numpy.npy",self.DATA)
+        
         
     def base_creating(self):
         mini_path = "./LOL_image/minimap/"
@@ -110,8 +124,9 @@ class DataCreator:
             self.width, self.height = width, height = outter.size
             minimap = Image.new("RGBA",(width, height))
             with Image.open(mini_path + "mini_inner.png") as inner:
-                self.Size = Size = 220
-                Inner = inner.resize((width - Size, height - Size))
+                self.Size = Size = 215
+                self.innerwidth, self.innerheight = width - Size -15, height - Size -15
+                Inner = inner.resize((self.innerwidth, self.innerheight))
                 minimap.paste(Inner, (Size, Size), Inner)
                 minimap.paste(outter, (0,0), outter)
                 #Inner.show()
@@ -122,6 +137,7 @@ class DataCreator:
 
     def hero_creating(self):
         hero_path = "./LOL_image/character/"
+        noise_path = "./LOL_image/noise/"
         self.position = []
         self.base_creating()
         minimap = self.base
@@ -136,11 +152,16 @@ class DataCreator:
         hero_ran = []
         for i in range(Num): hero_ran.append(secrets.choice(hero_list))
 
+        count = 0
         for hero in hero_ran:
-            width, height = secrets.randbelow(self.width - self.Size), secrets.randbelow(self.height - self.Size)
+            count+=1
+            if count > 5: Base = Image.open(noise_path + "leblanc_fake_allyteam.png").resize((34, 34))
+            else: Base = Image.open(noise_path + "leblanc_fake_enemyteam.png").resize((34, 34))
+            width, height = secrets.randbelow(self.innerwidth), secrets.randbelow( self.innerheight)
             width, height = width + self.Size, height + self.Size
             self.position.append([hero[:-4], width, height])
-            HERO = Image.open(hero_path + hero).resize((64, 64))
+            HERO = Image.open(hero_path + hero).resize((30, 30))
+            minimap.paste(Base, (width-2, height-2), Base)
             minimap.paste(HERO, (width, height), HERO)
 
         self.hero = minimap
@@ -158,20 +179,45 @@ class DataCreator:
                 break
 
         noise_ran = []
-        for i in range(self.noise): noise_ran.append(secrets.choice(noise_list))
+        for i in range(self.noise_len): noise_ran.append(secrets.choice(noise_list))
         print(noise_ran)
 
         for noise in noise_ran:
-            width, height = secrets.randbelow(self.width - self.Size), secrets.randbelow(self.height - self.Size)
+            width, height = secrets.randbelow(self.innerwidth), secrets.randbelow( self.innerheight)
             width, height = width + self.Size, height + self.Size
             NOISE = Image.open(noise_path + noise)
-            if NOISE.size[1] > 64: NOISE.resize((64, 64))
+            if NOISE.size[1] > 16: NOISE.resize((16, 16))
             minimap.paste(NOISE, (width, height), NOISE)
 
         self.noise = minimap
-        minimap.show()
         minimap.save(r"./Data/test_noise.png")
 
+    def ping_creating(self):
+        ping_path = "./LOL_image/ping/" 
+        self.noise_creating()
+        minimap = self.noise
+        ping_list = os.listdir(ping_path)
+
+        for i in range(len(ping_list)):
+            if ping_list[i] == ".DS_Store":
+                ping_list.pop(i)
+                break
+
+        ping_ran = []
+        for i in range(self.ping_len): ping_ran.append(secrets.choice(ping_list))
+        print(ping_ran)
+
+        for ping in ping_ran:
+            width, height = secrets.randbelow(self.innerwidth), secrets.randbelow(self.innerheight)
+            width, height = width + self.Size, height + self.Size
+            PING = Image.open(ping_path + ping).resize((34, 34))
+            #if PING.size[1] > 32: PING.resize((34, 34))
+            minimap.paste(PING, (width, height), PING)
+
+        self.ping = minimap
+        #minimap.show()
+        minimap.save(r"./Data/test_ping.png")
+        
 
 
 if __name__ == "__main__":
@@ -180,6 +226,7 @@ if __name__ == "__main__":
     Parser.add_argument('--amount', required=True ,type=int, help = "Gathering Img for creating data")
     Parser.add_argument('--noise', required=True ,type=int, help = "amount of noise at each png")
     Parser.add_argument('--overlap', required=True ,type=int, help = "percent of overlapped with character img")
+    Parser.add_argument('--ping', required=True ,type=int, help = "amount of ping at each png")
     Parser.add_argument('--gathering', required=False, type=bool, help = "Gathering Img for creating data")
 
     args = Parser.parse_args()
@@ -191,10 +238,16 @@ if __name__ == "__main__":
         gatherer.get_minimap_base()
         gatherer.get_minimap_noise()
 
-    creator = DataCreator(args.save, args.amount, args.noise, args.overlap)
+    creator = DataCreator(args.save, args.amount, args.noise, args.overlap, args.ping)
     #creator.base_creating()
     #creator.hero_creating()
-    creator.noise_creating()
+    #creator.noise_creating()
+
+    for _ in range(args.amount):
+        creator.ping_creating()
+        creator.stack()
+        
+    creator.saving()
 
     
     
